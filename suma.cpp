e0bf7e52dd1977320d16cd4a9fc902aa916eba4f
smcartagenaa <smcartagenaa@unal.edu.co>
2017-02-03 01:54:31 -0500

    CLion es la ley

---
+++ suma.cpp
@@ -0,0 +1,24 @@
+//
+// Created by sandra milena on 3/02/2017.
+//
+
+#include <iostream>
+
+using namespace std;
+
+float sumToN(int n){
+    float sum = 0.0;
+    for (int i = 1; i <= n ; ++i) {
+        sum += 1.0/i;
+        cout << i << "\t" << sum << endl;
+    }
+    return sum;
+}
+
+int main() {
+
+    sumToN(10);
+
+    return 0;
+}
+
